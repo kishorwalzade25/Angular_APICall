bebd74de3a7056d832928b7c8e65e22f0c19f71c
kishor_walzade <kishorwalzade25@gmail.com>
2025-06-26 22:37:15 +0530

    authservice

--- src/app/Service/auth.service.ts
+++ src/app/Service/auth.service.ts
@@ -4,57 +4,49 @@ import { catchError, Observable, of, tap } from "rxjs";
 import { IUser } from "../Models/event.models";
 import { ApiAddressService } from "./api_address.service";
 
-@Injectable(
-  {
-    providedIn: 'root'
-  })
-
-export class AuthService
-{
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
   currentUser!: IUser;
   Url: string = "";
   constructor(private apicall: ApiAddressService, private http: HttpClient) { }
 
-  loginUser(username: string, password: string):Observable<IUser|boolean>
-  {
+  loginUser(username: string, password: string): Observable<IUser | boolean> {
     this.Url = this.apicall.localaddress + "/api/authenticate";
-    let userbody: any =
-    {
-      userName:username,
-      password:password
-    }
-    let options =
-    {
-      headers: new HttpHeaders(
-        {
-          'Content-Type': 'application/json',
-        })
+    let userbody: any = {
+      userName: username,
+      password: password,
+    };
+    let options = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+      }),
     };
 
-    return this.http.post<IUser>(this.Url, userbody, options)
-      .pipe
-      (
-        tap((user: IUser) =>
-       {
-        this.currentUser =
-       {
-        userName: user.userName,
-        userLastName: user.userLastName,
-        tokenBearer: user.tokenBearer
-       };
+    return this.http
+      .post<IUser>(this.Url, userbody, options)
+      .pipe(
+        tap((user: IUser) => {
+          this.currentUser = {
+            userName: user.userName,
+            userLastName: user.userLastName,
+            tokenBearer: user.tokenBearer,
+          };
         })
       )
-      .pipe(catchError(err => { return of(false) }));
-    
+      .pipe(
+        catchError((err) => {
+          return of(false);
+        })
+      );
   }
 
-  isAuthenticated(): boolean
-  {
+  isAuthenticated(): boolean {
     return !!this.currentUser;
   }
 
-  getTokenBearer(): string
-  {
+  getTokenBearer(): string {
     return this.currentUser.tokenBearer;
   }
 }
